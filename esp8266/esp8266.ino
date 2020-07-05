3fe1fc08aa0cef7a613cbf57fa5ab6a93ea15d71
agungdh <agungdh@live.com>
2020-07-05 20:18:34 +0700

    bisa tapi tetep gatau kenapa aowkaowkawok

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,21 +1,37 @@
+#include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 
+bool onoff = true;
+
 SoftwareSerial NodeMCU(D2,D3);
 
-void setup(){
+void led() {
+	if (!onoff) {
+		digitalWrite(LED_BUILTIN, HIGH);
+	} else {
+		digitalWrite(LED_BUILTIN, LOW);
+	}
+}
+
+void setup() {
+	pinMode(LED_BUILTIN, OUTPUT);
+
+	led();
+
 	Serial.begin(9600);
 	NodeMCU.begin(4800);
+
 	pinMode(D2,INPUT);
 	pinMode(D3,OUTPUT);
 }
 
-void loop(){
+void loop() {
 	while(NodeMCU.available()>0){
-		float val = NodeMCU.parseFloat();
-		if(NodeMCU.read()== '\n'){
-			Serial.println(val);
-		}
+		NodeMCU.println("ledon\n");
+		delay(1000);
+		NodeMCU.println("ledoff\n");
+		delay(1000);
 	}
 	
 	delay(30);
--- uno/uno.ino
+++ uno/uno.ino
@@ -1,16 +1,79 @@
 #include <SoftwareSerial.h>
+
+#include <Arduino.h>
+#include "GravityTDS.h"
+
 SoftwareSerial ArduinoUno(3,2);
+#define TdsSensorPin A0
+GravityTDS gravityTds;
 
-void setup(){
-	Serial.begin(9600);
-	ArduinoUno.begin(4800);
+float tdsValue = 0;
 
+char rx_byte = 0;
+String rx_str = "";
+
+bool onoff = true;
+
+void led() {
+  if (onoff) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
 
-void loop(){
-	int i = 10;
-	ArduinoUno.print(i);
-	ArduinoUno.println("\n");
+void setup() {
+  pinMode(TdsSensorPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  led();
+
+  Serial.begin(9600);
+  ArduinoUno.begin(4800);
+
+  gravityTds.setPin(TdsSensorPin);
+  gravityTds.setAref(3.3);
+  gravityTds.setAdcRange(1024);
+  gravityTds.begin();
+}
+
+void loop() {
+	if (ArduinoUno.available() > 0) {
+	    rx_byte = ArduinoUno.read();
+	    
+	    if (rx_byte != '\n') {
+	      rx_str += rx_byte;
+	    }
+	    else {
+	    	if (rx_str == "tds") {
+				gravityTds.update();
+				tdsValue = gravityTds.getTdsValue();
+				ArduinoUno.print("tds:");
+				ArduinoUno.println(tdsValue,2);
+	    	} else if (rx_str == "ledcek") {
+				ArduinoUno.print("led:");
+				if (onoff) {
+					ArduinoUno.println("1");
+			    } else {
+					ArduinoUno.println("0");
+			    }
+	    	} else if (rx_str == "ledon") {
+    			onoff = true;
+    			led();
+	    	} else if (rx_str == "ledoff") {
+    			onoff = false;
+    			led();
+	    	} else if (rx_str.substring(0,7) == "tdscal:") {
+      			String temp;
+	    		temp = rx_str.substring(7);
+	    		gravityTds.customCalibrate(temp.toFloat());
+	    	} else {
+      			ArduinoUno.println(rx_str);
+	    	}
+
+	      rx_str = "";
+	    }
+	}
 
 	delay(30);
 }
